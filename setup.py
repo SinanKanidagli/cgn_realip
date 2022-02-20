f4cfd39b3dcce7170bce14f83f1868123db0cc90
Sinan Kanidağlı <82559809+SinanKanidagli@users.noreply.github.com>
2022-02-20 03:04:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ LONG_DESCRIPTION = 'Provides port forwarding for ip addresses in the cgn pool'
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
-    name="cgn_realip",                    # This is the name of the package
-    version=VERSION,                        # The initial release version
-    author="Sinan Kanidagli",                     # Full name of the author
+    name="cgn_realip",                    
+    version=VERSION,                        
+    author="Sinan Kanidagli",                     
     description=DESCRIPTION,
-    long_description=long_description,      # Long description read from the the readme file
+    long_description=long_description,     
     long_description_content_type="text/markdown",
-    packages=find_packages(),    # List of all python modules to be installed
+    packages=find_packages(),   
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["cgn_realip"],             # Name of the python package
-    package_dir={'':'cgn_realip/src'},     # Directory of the source code of the package
-    install_requires=['netifaces', 'js2py', 'requests']                     # Install other dependencies if any
+    ],                                      
+    python_requires='>=3.6',               
+    py_modules=["cgn_realip"],            
+    package_dir={'':'cgn_realip/src'},     
+    install_requires=['netifaces', 'js2py', 'requests']                    
 )
